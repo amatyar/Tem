b185baea0311803298c8e028ca8619fd4335f38a
Rabindra Amatya <amatyar@yahoo.com>
2017-01-18 21:00:15 -0500

    add new codes
    tempConvert is not allow to add so changed

---
+++ scripts/code.js
@@ -0,0 +1,6 @@
+function CelsiusToFahrenheit(Celsius) {
+var cTemp = celsius;
+var cToFahr = cTemp * 9/5 + 32;
+var answer = cTemp + " \xB0C is" + cToFahr + '\xB0F';
+document.getElementById('foo').innerHTML = answer;
+}
