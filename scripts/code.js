4e4223b1568f3d267f77164b473fd6131f5faee7
Rabindra Amatya <amatyar@users.noreply.github.com>
2017-01-18 21:11:45 -0500

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -4,3 +4,9 @@ var cToFahr = cTemp * 9/5 + 32;
 var answer = cTemp + " \xB0C is" + cToFahr + '\xB0F';
 document.getElementById('foo').innerHTML = answer;
 }
+function FahrenheitToCelsius(Fahrenheit) {
+var cTemp = celsius;
+var cToFahr = cTemp * 9/5 + 32;
+var answer = cTemp + " \xB0C is" + cToFahr + '\xB0F';
+document.getElementById('foo').innerHTML = answer;
+}
